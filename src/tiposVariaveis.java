fc30b9725de992adc75133570f5388d6cd07c941
Henrique Lacerda <henriquelacerda69@gmail.com>
2025-04-02 15:34:34 -0300

    alterando printIn

--- src/tiposVariaveis.java
+++ src/tiposVariaveis.java
@@ -1,5 +1,5 @@
 public class tiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("tipo de variáveis, World!");
+        System.out.println("tipo de variaveis, Java!");
     }
 }
