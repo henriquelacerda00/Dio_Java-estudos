595d87a7b99ec6cb13a1691d21200297d8ab1700
Henrique Lacerda <henriquelacerda69@gmail.com>
2025-04-15 09:31:28 -0300

    att repository

---
+++ src/operadores.java
@@ -0,0 +1,9 @@
+public class operadores {
+    public static void main(String[] args) throws Exception {
+        int numero = 5; 
+        //numero = numero + 1;
+
+        System.out.println(--numero);
+        //System.out.println(numero);
+    }
+}
